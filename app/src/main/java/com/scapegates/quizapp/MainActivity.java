195823a4507ae093e34193c4dfffd7e6af46117a
Mher <poghosyanUSC@gmail.com>
2016-07-05 20:18:20 -0700

    Set up layout for main activity and added functionality to submit button. Submit starts a new activity to display the results.

--- app/src/main/java/com/scapegates/quizapp/MainActivity.java
+++ app/src/main/java/com/scapegates/quizapp/MainActivity.java
@@ -1,13 +1,22 @@
 package com.scapegates.quizapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    int score, numQuestions;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void submit(View view) {
+        Intent showAnswer = new Intent(this, ResultsActivity.class);
+        showAnswer.putExtra("QUESTIONS", numQuestions);
+        showAnswer.putExtra("SCORE", score);
+        startActivity(showAnswer);
     }
 }
