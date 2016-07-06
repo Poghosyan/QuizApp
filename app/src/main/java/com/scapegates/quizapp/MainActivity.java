889d452cbe4eb992d9bf3046dc50937ea12701b7
Mher <poghosyanUSC@gmail.com>
2016-07-05 23:36:32 -0700

    Added functionality to get right answers and display score in ResultsActivity

--- app/src/main/java/com/scapegates/quizapp/MainActivity.java
+++ app/src/main/java/com/scapegates/quizapp/MainActivity.java
@@ -4,19 +4,47 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
-    int score, numQuestions;
+    int score;
+    final int QUESTIONS = 4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        score = 0;
     }
 
     public void submit(View view) {
+        RadioButton radio = (RadioButton) findViewById(R.id.right_answer_radio);
+        CheckBox check1 = (CheckBox) findViewById(R.id.right_answer_check1);
+        CheckBox check2 = (CheckBox) findViewById(R.id.right_answer_check2);
+        EditText firstText = (EditText) findViewById(R.id.text_answer_first);
+        EditText secondText = (EditText) findViewById(R.id.text_answer_second);
+
+        if(radio.isChecked()) {
+            score++;
+        }
+
+        if(check1.isChecked() && check2.isChecked()) {
+            score++;
+        }
+
+        if(firstText != null && firstText.getText().toString().equalsIgnoreCase(getString(R.string.text_answer_first))) {
+            score++;
+        }
+
+        if(secondText!= null && secondText.getText().toString().equalsIgnoreCase(getString(R.string.text_answer_second))) {
+            score++;
+        }
+
         Intent showAnswer = new Intent(this, ResultsActivity.class);
-        showAnswer.putExtra("QUESTIONS", numQuestions);
-        showAnswer.putExtra("SCORE", score);
+        showAnswer.putExtra(getString(R.string.questions), QUESTIONS);
+        showAnswer.putExtra(getString(R.string.score), score);
         startActivity(showAnswer);
+        score = 0;
     }
 }
--- app/src/main/java/com/scapegates/quizapp/ResultsActivity.java
+++ app/src/main/java/com/scapegates/quizapp/ResultsActivity.java
@@ -2,12 +2,18 @@ package com.scapegates.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ResultsActivity extends AppCompatActivity {
-
+    int score, questions;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        questions = getIntent().getIntExtra(getString(R.string.questions), 0);
+        score = getIntent().getIntExtra(getString(R.string.score), 0);
+        String scoreStr = "" + score + "/" + questions;
+        ((TextView) findViewById(R.id.score)).setText(scoreStr);
     }
 }
